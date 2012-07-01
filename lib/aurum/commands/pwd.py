ae36cdb73c0b8ffb77400a3497c1242de55892df
elazar <tobias382@gmail.com>
2012-06-30 20:45:18 -0500

    Fixed spacing formatting and handles of DSNs with database specifications in the pwd command

--- lib/aurum/commands/pwd.py
+++ lib/aurum/commands/pwd.py
@@ -1,20 +1,26 @@
 from .. import command
 
+from sqlalchemy.engine.url import make_url
+
 class Pwd(command.Command):
 
     def do_all(self, params):
         context = self.context
         context_type = context.get_type()
         if context_type == "global":
-            print "/",
+            path = "/"
         else:
+            path = ""
             if context_type in ("server", "database", "table", "column"):
-                print "/" + context.dsn,
+                path += "/" + context.dsn
             if context_type in ("database", "table", "column"):
-                print "/" + context.database,
+                parsed_dsn = make_url(self.config.dsn[context.dsn])
+                database = parsed_dsn.database
+                if database == None:
+                    path += "/" + context.database
             if context_type in ("table", "column"):
-                print "/" + context.table,
+                path += "/" + context.table
             if context_type == "column":
-                print "/" + context.column,
-        print
+                path += "/" + context.column
+        print path
         return False
